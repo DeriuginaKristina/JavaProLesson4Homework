0d61353210bde5f492d4115f13ee1f7596a2f157
Kristina Deriugina <derkristina258@gmail.com>
2024-06-25 21:42:58 +0200

    Added Lesson 13 integration Tests

---
+++ src/main/java/de/ait_tr/g_33_shop/service/ProductServiceImpl.java
@@ -0,0 +1,94 @@
+package de.ait_tr.g_33_shop.service;
+
+import de.ait_tr.g_33_shop.domain.dto.ProductDto;
+import de.ait_tr.g_33_shop.domain.entity.Product;
+import de.ait_tr.g_33_shop.exception_handling.exceptions.FirstTestException;
+import de.ait_tr.g_33_shop.exception_handling.exceptions.FourthTestException;
+import de.ait_tr.g_33_shop.exception_handling.exceptions.SecondTestException;
+import de.ait_tr.g_33_shop.exception_handling.exceptions.ThirdTestException;
+import de.ait_tr.g_33_shop.repository.ProductRepository;
+import de.ait_tr.g_33_shop.service.interfaces.ProductService;
+import de.ait_tr.g_33_shop.service.mapping.ProductMappingService;
+import org.apache.catalina.webresources.FileResourceSet;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private ProductRepository repository;
+    private ProductMappingService mappingService;
+
+    public ProductServiceImpl(ProductRepository repository, ProductMappingService mappingService) {
+        this.repository = repository;
+        this.mappingService = mappingService;
+    }
+
+    @Override
+    public ProductDto save(ProductDto dto) {
+        Product entity = mappingService.mapDtoToEntity(dto);
+        try {
+            repository.save(entity);
+        } catch (Exception e) {
+            throw new FourthTestException(e.getMessage());
+        }
+
+        return mappingService.mapEntityToDto(entity);
+    }
+    @Override
+    public List<ProductDto> getAllActiveProducts() {
+        return repository.findAll()
+                .stream()
+                .filter(Product::isActive)
+                .map(mappingService::mapEntityToDto)
+                .toList();
+    }
+
+    @Override
+    public ProductDto getById(Long id) {
+        Product product = repository.findById(id).orElse(null);
+
+        if (product != null && product.isActive()) {
+            return mappingService.mapEntityToDto(product);
+        }
+
+        throw new ThirdTestException("This is third test exception");
+    }
+
+    @Override
+    public ProductDto update(ProductDto product) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+
+    @Override
+    public void deleteByTitle(String title) {
+
+    }
+
+    @Override
+    public void restoreById(Long id) {
+
+    }
+
+    @Override
+    public long getAllActiveProductsQuantity() {
+        return 0;
+    }
+
+    @Override
+    public BigDecimal getAllActiveProductsTotalPrice() {
+        return null;
+    }
+
+    @Override
+    public BigDecimal getAllActiveProductsAveragePrice() {
+        return null;
+    }
+}
